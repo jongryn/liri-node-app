6ee4ffb8060b7d601784457bd2f4ec55d140fbed
Jonathan Gryn <jonathangryn@Jonathans-MacBook-Pro.local>
2017-09-24 23:52:33 -0700

    safety commit

--- liri.js
+++ liri.js
@@ -2,8 +2,8 @@
 // Created: Aug. 22, 2017 5:25 PM
 // Author: Jonathan Gryn
 // Revisions: Jon (8/22/17) - Added JS
-//            Jon (8/23/17) - Got Spotify API to start responding
-//            Jon (8/28/17) - Configured OMDb API to run a function
+//            Jon (8/23/17) - Tried to get Twitter to work
+//            Jon (8/29/17) - Got Twitter's API to work, got Spotify API to start responding, & configured OMDb API to run a function
 //            Jon (9/3/17) - Added OMDb API and completed bonus
 */
 
